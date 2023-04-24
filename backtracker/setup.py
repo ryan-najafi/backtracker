b1c6219e7bb7065dfe111ad02ba388596e214170
m1najafi <amotions@gmail.com>
2023-04-23 23:13:49 -0700

    add test

---
+++ backtracker/setup.py
@@ -0,0 +1,29 @@
+from setuptools import setup, find_packages
+
+VERSION = '0.0.1' 
+DESCRIPTION = 'Backtracking Python package'
+LONG_DESCRIPTION = 'Generic implementation of a backtracker algorithm based on Steven Skiena in python.'
+
+# Setting up
+setup(
+       # the name must match the folder name 'verysimplemodule'
+        name="backtracker", 
+        version=VERSION,
+        author="Ryan Najafi",
+        author_email="<ryan.m.najafi@email.com>",
+        description=DESCRIPTION,
+        long_description=LONG_DESCRIPTION,
+        packages=find_packages(),
+        install_requires=[], # add any additional packages that 
+        # needs to be installed along with your package. Eg: 'caer'
+        
+        keywords=['python', 'backtracker'],
+        classifiers= [
+            "Development Status :: 3 - Alpha",
+            "Intended Audience :: Education",
+            "Programming Language :: Python :: 2",
+            "Programming Language :: Python :: 3",
+            "Operating System :: MacOS :: MacOS X",
+            "Operating System :: Microsoft :: Windows",
+        ]
+)
